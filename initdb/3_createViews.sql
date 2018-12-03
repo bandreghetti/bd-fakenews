76029ccd6994577381f96686f878dcee64f6037c
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-12-03 18:33:05 -0200

    Add "distinct" keyword

--- initdb/3_createViews.sql
+++ initdb/3_createViews.sql
@@ -1,6 +1,6 @@
 DROP VIEW IF EXISTS v_todasnoticias, v_noticia_midia, v_partidos_coligacao;
 CREATE VIEW v_todasnoticias AS
-    SELECT t_noticia.codigo as codNoticia,
+    SELECT DISTINCT t_noticia.codigo as codNoticia,
 		   manchete,
 		   corpo,
 		   submetidapor,
@@ -9,19 +9,14 @@ CREATE VIEW v_todasnoticias AS
 		   datanascimento,
 		   concorreem,
 		   t_cargo.nome as cargo,
-           t_coligacao.nome as coligacao,
            t_candidato.siglaPartido as partido
 	FROM t_noticia,
          t_candidato,
          r_candidato_noticia,
          t_cargo,
-         t_coligacao,
-         r_partido_coligacao
 	WHERE r_candidato_noticia.cpfCandidato = t_candidato.cpf
 	  AND r_candidato_noticia.codNoticia = t_noticia.codigo
 	  AND t_cargo.codigo = t_candidato.codCargo
-      AND r_partido_coligacao.siglaPartido = t_candidato.siglaPartido
-      AND r_partido_coligacao.codColigacao = t_coligacao.codigo;
 
 
 CREATE VIEW v_noticia_midia AS
