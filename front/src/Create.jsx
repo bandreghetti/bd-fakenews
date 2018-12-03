8c3b5ad17f5ce3c5d82fe7eefce7ac475180580f
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-12-03 00:03:55 -0200

    Begin to build JSON for posting news

--- front/src/Create.jsx
+++ front/src/Create.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   withStyles,
   Typography,
@@ -10,43 +10,42 @@ import {
   Button,
   CardActions,
   Collapse
-} from '@material-ui/core';
+} from "@material-ui/core";
 
-
-const styles = (theme) => ({
-    root: {
-      backgroundColor: '#c3c9d6',
-      minHeight: '100vh',
-      display: 'flex',
-      flexdirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: 'calc(10px + 2vmin)',
-      color: 'white',
-    },
-    title: {
-      fontSize: 14,
-    },
-    card: {
-      minWidth: 800,
-      maxWidth: 1000,
-    },
-    textField: {
-      marginLeft: theme.spacing.unit,
-      marginRight: theme.spacing.unit,
-    },
-    button: {
-      margin: theme.spacing.unit,
-    },
-    input: {
-      display: 'none',
-    },
-    sendButton: {
-      justifyContent: 'flex-end',
-      display: 'flex',
-      marginLeft: 'auto',
-      paddingRight: 35,
-    }
+const styles = theme => ({
+  root: {
+    backgroundColor: "#c3c9d6",
+    minHeight: "100vh",
+    display: "flex",
+    flexdirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white"
+  },
+  title: {
+    fontSize: 14
+  },
+  card: {
+    minWidth: 800,
+    maxWidth: 1000
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  input: {
+    display: "none"
+  },
+  sendButton: {
+    justifyContent: "flex-end",
+    display: "flex",
+    marginLeft: "auto",
+    paddingRight: 35
+  }
 });
 
 class CreateFakeNews extends React.Component {
@@ -54,121 +53,128 @@ class CreateFakeNews extends React.Component {
     super(props);
     this.state = {
       files: []
-    }
+    };
   }
 
-  handleFileUpload = (event) => {
-    this.setState({files: event.target.files})
-  }
+  handleFileUpload = event => {
+    this.setState({ files: event.target.files });
+  };
+
+  handleChange = (event, field) => {
+    this.setState({ [field]: event.target.value });
+  };
 
   send = () => {
-
-  }
-  render(){
+    let submitJSON = {};
+    console.log(this.state);
+    submitJSON.user = {
+      email: this.state.email,
+      name: this.state.username
+    };
+    submitJSON.new = {
+      headline: this.state.title,
+      body: this.state.body,
+      submittedBy: this.state.email
+    };
+    submitJSON.publi = {
+      link: this.state.link,
+      author: this.state.author,
+      codVeiculo: 1
+    };
+    console.log(submitJSON);
+  };
+  render() {
     const { classes } = this.props;
     const { files } = this.state;
     const fields = [
-      {field: 'username', label: 'Nome de Usuário'},
-      {field: 'email', label: 'Email'},
-      {field: 'candidate', label: 'Candidato'},
-      {field: 'vehicle', label: 'Veículo de mídia'},
-      {field: 'link', label: 'Link'},
-      {field: 'author', label: 'Autor'},
+      { field: "username", label: "Nome de Usuário" },
+      { field: "email", label: "Email" },
+      { field: "candidate", label: "Candidato" },
+      { field: "vehicle", label: "Veículo de mídia" },
+      { field: "link", label: "Link" },
+      { field: "author", label: "Autor" }
     ];
     let filesRecvd = null;
     let hasFiles = false;
     if (files.length > 0) {
-      filesRecvd = (
-        Object.keys(files).map((fileKey) => (
-            <Typography
-              key={fileKey}
-            >
-              {files[fileKey].name}
-            </Typography>
-        ))
-      )
+      filesRecvd = Object.keys(files).map(fileKey => (
+        <Typography key={fileKey}>{files[fileKey].name}</Typography>
+      ));
       hasFiles = true;
     }
-    return(
-        <div className={classes.root}>
-          <Paper>
-            <Card className={classes.card}>
-              <CardContent>
-                <Typography
-                  className={classes.title}
-                  color="textSecondary"
-                  gutterBottom
-                  align="left"
-                >
-                  Submeter nova fake news taokei
-                </Typography>
-                <Grid container>
-                  {fields.map((field, idx) => (
-                    <Grid
-                      item
-                      xs={6}
-                      key={`${idx}-grid`}
-                    >
-                      <TextField
-                        fullWidth
-                        label={field.label}
-                        style ={{width: '95%'}}
-                        onChange={(event) => this.handleChange(event, field.field)}
-                      />
-                    </Grid>
-                  ))}
-                  <Grid item xs={12}>
-                    <TextField
-                      style={{width: '97.5%'}}
-                      label="Título"
-                      onChange={(event) =>  this.handleChange(event, 'title')}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      label="Corpo da Matéria"
-                      multiline
-                      rows="6"
-                      className={classes.textField}
-                      margin="normal"
-                      style={{width: '97.5%'}}
-                    />
-                  </Grid>
-                </Grid>
-              </CardContent>
-              <Collapse
-                in={hasFiles}
+    return (
+      <div className={classes.root}>
+        <Paper>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom
+                align="left"
               >
-                  {filesRecvd}
-              </Collapse>
-              <CardActions className={classes.sendButton} disableActionSpacing>
-                <input
-                  accept="image/*, video/*"
-                  className={classes.input}
-                  id="flat-button-file"
-                  multiple
-                  type="file"
-                  onChange={this.handleFileUpload}
-                />
-                <label htmlFor="flat-button-file">
-                  <Button variant="contained"  color="secondary" component="span" className={classes.button}>
-                    Upload mídias
-                  </Button>
-                </label>
+                Submeter nova fake news taokei
+              </Typography>
+              <Grid container>
+                {fields.map((field, idx) => (
+                  <Grid item xs={6} key={`${idx}-grid`}>
+                    <TextField
+                      fullWidth
+                      label={field.label}
+                      style={{ width: "95%" }}
+                      onChange={event => this.handleChange(event, field.field)}
+                    />
+                  </Grid>
+                ))}
+                <Grid item xs={12}>
+                  <TextField
+                    style={{ width: "97.5%" }}
+                    label="Título"
+                    onChange={event => this.handleChange(event, "title")}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    label="Corpo da Matéria"
+                    multiline
+                    rows="6"
+                    className={classes.textField}
+                    margin="normal"
+                    style={{ width: "97.5%" }}
+                    onChange={event => this.handleChange(event, "body")}
+                  />
+                </Grid>
+              </Grid>
+            </CardContent>
+            <Collapse in={hasFiles}>{filesRecvd}</Collapse>
+            <CardActions className={classes.sendButton} disableActionSpacing>
+              <input
+                accept="image/*, video/*"
+                className={classes.input}
+                id="flat-button-file"
+                multiple
+                type="file"
+                onChange={this.handleFileUpload}
+              />
+              <label htmlFor="flat-button-file">
                 <Button
                   variant="contained"
-                  color="primary"
-                  onClick={this.send}
+                  color="secondary"
+                  component="span"
+                  className={classes.button}
                 >
-                    Enviar!
+                  Upload mídias
                 </Button>
-              </CardActions>
-            </Card>
-          </Paper>
-        </div>
+              </label>
+              <Button variant="contained" color="primary" onClick={this.send}>
+                Enviar!
+              </Button>
+            </CardActions>
+          </Card>
+        </Paper>
+      </div>
     );
   }
-
-};
+}
 
 export default withStyles(styles)(CreateFakeNews);
